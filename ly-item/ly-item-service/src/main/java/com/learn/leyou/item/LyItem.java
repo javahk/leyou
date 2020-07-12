b6c374a79eb6e29fdb928a96fa101501f5cf6793
rui.wang <rui.wang@shangri-la.com>
2020-07-12 18:20:09 +0800

    部分功能完成

--- ly-item/ly-item-service/src/main/java/com/learn/leyou/item/LyItem.java
+++ ly-item/ly-item-service/src/main/java/com/learn/leyou/item/LyItem.java
@@ -2,10 +2,12 @@ package com.learn.leyou.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.learn.leyou.item.mapper")
 public class LyItem {
 
     public static void main(String[] args) {
